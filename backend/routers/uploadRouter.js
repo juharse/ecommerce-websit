f98d1cdb105554372667346a45bccad1781121a2
juhar <ebraseid44@gmail.com>
2024-04-23 05:27:32 -0700

    added

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -6,7 +6,7 @@ const uploadRouter = express.Router();
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, 'images/');
+    cb(null, 'uploads/');
   },
   filename(req, file, cb) {
     cb(null, `${Date.now()}.jpg`);
