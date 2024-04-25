5bffc94aca9a7d2e929a90337d34b632347a832b
juhar <ebraseid44@gmail.com>
2024-04-25 06:53:48 -0700

    fetch

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -1,6 +1,8 @@
 import multer from 'multer';
 import express from 'express';
 import { isAuth } from '../utils.js';
+import { promises as fs } from 'fs';
+import path from 'path';
 
 const uploadRouter = express.Router();
 
@@ -18,11 +20,30 @@ const upload = multer({ storage });
 uploadRouter.post('/', isAuth, upload.single('image'), (req, res) => {
   res.send(`/${req.file.path}`);
 });
-uploadRouter.get('/:imageName', (req, res) => {
+/*uploadRouter.get('/:imageName', (req, res) => {
   const { imageName } = req.params;
   const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${imageName}`;
-  console.log(imageUrl)
   res.json({ imageUrl });
+});*/
+uploadRouter.get('/:imageName', async (req, res) => {
+  const { imageName } = req.params;
+
+  try {
+    // Assuming images are stored in the 'images' directory
+    const imagePath = path.join(__dirname, 'uploads', imageName);
+
+    // Read the image file
+    const imageData = await fs.readFile(imagePath);
+
+    // Set the content type header
+    res.setHeader('Content-Type', 'image/jpeg'); // Adjust the content type based on your image format
+
+    // Send the image data as the response
+    res.send(imageData);
+  } catch (error) {
+    console.error('Error fetching image:', error);
+    res.status(404).json({ error: 'Image not found' });
+  }
 });
 
 export default uploadRouter;
--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,8 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,  Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-    next();
+  res.header('Access-Control-Allow-Credentials', true);
+      next();
   });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
